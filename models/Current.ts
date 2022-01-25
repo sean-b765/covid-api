29ddcd3c39a5e6cfb58802cf5c6e6c702981b92e
sean-b765 <61043071+sean-b765@users.noreply.github.com>
2022-01-25 10:02:49 +0800

    Update Current schema

--- models/Current.ts
+++ models/Current.ts
@@ -1,16 +1,40 @@
 import mongoose from 'mongoose'
 
+export interface ICurrent extends mongoose.Document {
+	location: string
+	provinces: [
+		{
+			county: { type: String; required: false }
+			zip: { type: String; required: false }
+			state: { type: String; required: false }
+			lat: { type: String }
+			lng: { type: String }
+			cumulative: { type: String }
+			deaths: { type: String }
+			recovered: { type: String }
+		}
+	]
+	lat: { type: String; required: false }
+	lng: { type: String; required: false }
+}
+
 // FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key,Incident_Rate,Case_Fatality_Ratio
 const currentSchema = new mongoose.Schema({
-	zip: { type: String, required: false },
-	county: { type: String, required: false },
-	state: { type: String },
-	country: { type: String },
-	lat: { type: String },
-	lng: { type: String },
-	cumulative: { type: String },
-	deaths: { type: String },
-	recovered: { type: String },
+	location: String,
+	provinces: [
+		{
+			county: { type: String, required: false },
+			zip: { type: String, required: false },
+			state: { type: String, required: false },
+			lat: { type: String },
+			lng: { type: String },
+			cumulative: { type: String },
+			deaths: { type: String },
+			recovered: { type: String },
+		},
+	],
+	lat: { type: String, required: false },
+	lng: { type: String, required: false },
 })
 
-export default mongoose.model('Current', currentSchema)
+export default mongoose.model<ICurrent>('Current', currentSchema)
