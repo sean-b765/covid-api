fc54569544c1af3c03492008305de8a2fa816041
sean-b765 <61043071+sean-b765@users.noreply.github.com>
2022-01-26 13:57:03 +0800

    Update home routes

--- dist/routes/home.js
+++ dist/routes/home.js
@@ -8,13 +8,28 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const countryCodes_1 = __importDefault(require("../lib/countryCodes"));
 const router = (0, express_1.Router)();
 router.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.status(200).json({
-        message: 'Hit /history/{{location}} to get historical data of the specified location.  e.g. /history/North America, /history/World, /history/USA',
-    });
+    res.status(200).send(`<h1>Hit /history/{{location}} to get historical data of the specified location. /current/{{location}} will return the up-to-date statistics.</h1>
+			<p>/locations lists all the available locations.</p>
+			`);
+}));
+router.get('/locations', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.status(200).json(countryCodes_1.default.map((value) => {
+        return {
+            name: value.db_name,
+            lat: value.latitude,
+            lng: value.longitude,
+            alpha2: value.alpha2code,
+            alpha3: value.alpha3code,
+        };
+    }));
 }));
 exports.default = router;
 //# sourceMappingURL=home.js.map
--- routes/home.ts
+++ routes/home.ts
@@ -1,12 +1,27 @@
 import { Request, Response, Router } from 'express'
+import countryCodes from '../lib/countryCodes'
 
 const router = Router()
 
 router.get('/', async (req: Request, res: Response) => {
-	res.status(200).json({
-		message:
-			'Hit /history/{{location}} to get historical data of the specified location.  e.g. /history/North America, /history/World, /history/USA',
-	})
+	res.status(200).send(
+		`<h1>Hit /history/{{location}} to get historical data of the specified location. /current/{{location}} will return the up-to-date statistics.</h1>
+			<p>/locations lists all the available locations.</p>
+			`
+	)
+})
+router.get('/locations', async (req: Request, res: Response) => {
+	res.status(200).json(
+		countryCodes.map((value) => {
+			return {
+				name: value.db_name,
+				lat: value.latitude,
+				lng: value.longitude,
+				alpha2: value.alpha2code,
+				alpha3: value.alpha3code,
+			}
+		})
+	)
 })
 
 export default router
